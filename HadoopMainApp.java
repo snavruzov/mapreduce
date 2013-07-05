0c12d1faf8016692a27c7e269dcdb1408eebd22d
Sardor Navruzov <navruzov@gmail.com>
2013-07-05 17:24:05 +0500

    Create HadoopMainApp.java

---
+++ HadoopMainApp.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Intellij IDEA.
+ * User: Sardor Navruzov
+ * Date: 4/22/13
+ * Time: 4:27 PM
+ */
+public class HadoopMainApp
+{    
+
+    public static void main(String[] args) throws Exception {    
+
+        CityTemperature ct= new CityTemperature();
+        System.out.println(ct.getReduceDone());
+
+
+    }    
+
+}
